06c6cd8315d6773757c9336b78e2846cb181eeba
Dev. Jose Parra <japch756@gmail.com>
2026-05-18 16:34:38 -0400

    Preparando deploy Render

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -8,5 +8,18 @@ def home():
     return "Bot funcionando correctamente"
 
 
+@app.route("/webhook", methods=["GET", "POST"])
+def webhook():
+
+    if request.method == "GET":
+        return "Webhook conectado correctamente"
+
+    if request.method == "POST":
+        data = request.json
+        print(data)
+
+        return "ok", 200
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
