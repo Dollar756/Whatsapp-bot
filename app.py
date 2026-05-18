d67cac3a7d7cf6cccddda0314d7f7024f35908f2
Dev. Jose Parra <japch756@gmail.com>
2026-05-18 17:25:41 -0400

    Webhook meta verification

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+import requests
 from flask import Flask, request
 
 app = Flask(__name__)
 
 VERIFY_TOKEN = "supportai"
+
+ACCESS_TOKEN = "EAAOrnFcpWz8BRXDRNmXtE1WI236lQi85FZByYzDMmQZB6oDkXEbJfyU5d00TGR4gjCmHYR2iZCK6Q8Yx9cEWLpcKXYCvULVkaBzhH4ZBz6gqoKMtZCB3dqs8gV13o8jYUZAvUMpFZB3ZCbe9aZBR0b9tzukvFwOIfRN0liFuHDEiknnVRNNtjNMkV90ZAe5h4jpVi5CqLvg5R8ZAh5gwCN719KBgc4rZCn59suifQHMfMzy6u1FZCjK5VkCThwV34jZAUmisW2Whq5S5OPZBETvknDTxTwG7LndTPbvlPrGDwZDZD"
+
+PHONE_NUMBER_ID = "1238581532660962"
 
 
 @app.route("/")
@@ -28,7 +33,36 @@ def webhook():
         data = request.json
         print(data)
 
+        try:
+            message = data["entry"][0]["changes"][0]["value"]["messages"][0]
+            from_number = message["from"]
+            text = message["text"]["body"]
+
+            send_message(from_number, f"Recibí tu mensaje: {text}")
+
+        except:
+            pass
+
         return "ok", 200
+
+
+def send_message(to, text):
+
+    url = f"https://graph.facebook.com/v25.0/{PHONE_NUMBER_ID}/messages"
+
+    headers = {
+        "Authorization": f"Bearer {ACCESS_TOKEN}",
+        "Content-Type": "application/json",
+    }
+
+    data = {
+        "messaging_product": "whatsapp",
+        "to": to,
+        "type": "text",
+        "text": {"body": text},
+    }
+
+    requests.post(url, headers=headers, json=data)
 
 
 if __name__ == "__main__":
